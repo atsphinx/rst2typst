c2accfd5a54d1591f88d7179c970f4deb7d03b8d
Kazuya Takei <myself@attakei.net>
2026-02-04 04:00:15 +0900

    feat: Implement for text content and comment node

--- src/rst2typst/writer.py
+++ src/rst2typst/writer.py
@@ -2,8 +2,13 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from docutils import nodes
 from docutils.writers import Writer as BaseWriter
+
+if TYPE_CHECKING:
+    from typing import Any
 
 
 class Writer(BaseWriter):
@@ -20,11 +25,29 @@ class Writer(BaseWriter):
 class TypstTranslator(nodes.NodeVisitor):
     # NOTE: Guard for NotImplementedError for unknown nodes. Remove as soon as possible.
     class WarningOnly:
-        def __contains__(self, item):
+        def __contains__(self, item: Any):
             return True
 
-    def __init__(self, document):
+    def __init__(self, document: nodes.document):
         super().__init__(document)
         self.document = document
         self.optional = self.WarningOnly()
         self.body = []
+
+    # --
+    # For base syntax
+    # --
+
+    def visit_Text(self, node: nodes.Text):
+        self.body.append(node.astext())
+
+    def depart_Text(self, node: nodes.Text):
+        pass
+
+    # --
+    # For doctree nodes
+    # --
+
+    # Refs: https://www.docutils.org/docs/ref/doctree.html#comment
+    def visit_comment(self, node: nodes.comment):
+        raise nodes.SkipNode
