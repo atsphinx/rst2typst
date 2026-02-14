e66e0462eb6ba5859ed9abed0d92a3e11567b1b8
Kazuya Takei <myself@attakei.net>
2026-02-15 01:10:10 +0900

    refactor: Sort visitors and departers methods

--- src/rst2typst/writer.py
+++ src/rst2typst/writer.py
@@ -101,9 +101,9 @@ class TypstTranslator(nodes.NodeVisitor):
 
         return _block_on_structural
 
-    # --
-    # For base syntax
-    # --
+    # ========================================
+    # The visitors and deparers for plain text
+    # ========================================
 
     def visit_Text(self, node: nodes.Text):
         self.body.append(f"\n{self._hi.indent}".join(node.astext().split("\n")))
@@ -111,32 +111,25 @@ class TypstTranslator(nodes.NodeVisitor):
     def depart_Text(self, node: nodes.Text):
         pass
 
-    # For doctree nodes
-    # =================
+    # ===========================================================
+    # The visitors and deparers for basic reStructuredText syntax
+    # ===========================================================
     #
-    # This section collects visit/depart methods for built-in doctree nodes.
-    # Methods are sorted by these rules:
+    # They are sorted by these rules:
     #
-    #   * The order of "Element Categories" of `doctree page`_.
-    #   * The order of classification of each categories: "empty", "simple", "compound".
-    #   * Alphabetical order of nodes.
+    #   * The order from "Syntax details" of `reStructuredText Markup Specification`_.
+    #   * When the node has children node types, write it nearby the parent node type.
     #   * ``visit_`` is first, and ``depart_`` is second if it is exists.
     #
-    # It adds reference to pages of typst before definition of each ``visit_`` methods.
-    #
-    # .. _doctree page: https://www.docutils.org/docs/ref/doctree.html
+    # .. _reStructuredText Markup Specification: https://docutils.org/docs/ref/rst/restructuredtext.html
 
-    # Root Element
-    # ------------
-
+    # Document Structure
+    # ==================
     def visit_document(self, node: nodes.document):
         pass
 
     def depart_document(self, node: nodes.document):
         pass
-
-    # Structural Elements
-    # -------------------
 
     def visit_section(self, node: nodes.section):
         self.section_level += 1
@@ -148,15 +141,6 @@ class TypstTranslator(nodes.NodeVisitor):
 
     def depart_section(self, node: nodes.section):
         self.section_level -= 1
-
-    # Structural Subelements
-    # ----------------------
-
-    def visit_docinfo(self, node: nodes.docinfo):
-        self._hi.push("/ ")
-
-    def depart_docinfo(self, node: nodes.docinfo):
-        self._hi.pop()
 
     # Refs: https://typst.app/docs/reference/model/title/
     def visit_title(self, node: nodes.title):
@@ -172,107 +156,8 @@ class TypstTranslator(nodes.NodeVisitor):
         else:
             self.body.append("\n\n")
 
-    # Decorative Elements
-    # -------------------
-    #
-    # Currently, there are not defined.
-
-    # Bibliographic Elements
-    # ----------------------
-    #
-    # Currently, there are not defined.
-
-    def _visit_bibliographic(self, node: nodes.Bibliographic):
-        title = node.__class__.__name__.title()
-        self.body.append(f"{self._hi.prefix}{title}: ")
-
-    def _depart_bibliographic(self, node: nodes.Bibliographic):
-        self.body.append("\n")
-
-    # Refs: https://typst.app/docs/reference/model/terms/
-    visit_address = _visit_bibliographic
-    depart_address = _depart_bibliographic
-    visit_author = _visit_bibliographic
-    depart_author = _depart_bibliographic
-    visit_contact = _visit_bibliographic
-    depart_contact = _depart_bibliographic
-    visit_copyright = _visit_bibliographic
-    depart_copyright = _depart_bibliographic
-    visit_date = _visit_bibliographic
-    depart_date = _depart_bibliographic
-    visit_organization = _visit_bibliographic
-    depart_organization = _depart_bibliographic
-    visit_revision = _visit_bibliographic
-    depart_revision = _depart_bibliographic
-    visit_status = _visit_bibliographic
-    depart_status = _depart_bibliographic
-    visit_version = _visit_bibliographic
-    depart_version = _depart_bibliographic
-
     # Body Elements
-    # -------------
-
-    def visit_image(self, node: nodes.image):
-        # FIXME: Implement is too complex.
-        prefix = self._hi.indent
-        suffix = "\n\n"
-        if isinstance(node.parent, nodes.figure):
-            prefix = f"{prefix}"
-            suffix = "]"
-        elif isinstance(node.parent, nodes.reference):
-            prefix = f"\n{prefix}"
-            suffix = "]"
-        self.body.append(f"{prefix}#image(\n")
-        self._hi.push("  ")
-        self.body.append(f'{self._hi.indent}"{node["uri"]}",\n')
-        if "alt" in node:
-            self.body.append(f'{self._hi.indent}alt: "{node["alt"]}",\n')
-        if "width" in node:
-            self.body.append(f"{self._hi.indent}width: {node['width']},\n")
-        self._hi.pop()
-        self.body.append(f"{self._hi.indent}){suffix}")
-        if isinstance(node.parent, nodes.reference):
-            self._hi.pop()
-
-    def visit_comment(self, node: nodes.comment):
-        raise nodes.SkipNode
-
-    @block_on_structural
-    def visit_definition_list(self, node: nodes.definition_list):
-        self._hi.push("/ ")
-
-    def depart_definition_list(self, node: nodes.definition_list):
-        self._hi.pop()
-
-    # Refs: https://typst.app/docs/reference/model/terms/
-    @block_on_structural
-    def visit_field_list(self, node: nodes.field_list):
-        self.body.append("#table(\n")
-        self._hi.push("  ")
-        self.body.append(f"{self._hi.indent}columns: (1fr, auto),\n")
-
-    def depart_field_list(self, node: nodes.field_list):
-        self._hi.pop()
-        self.body.append(")")
-
-    # Refs: https://typst.app/docs/reference/text/raw/
-    def visit_literal_block(self, node: nodes.literal_block):
-        if "code" in node["classes"]:
-            code = node["classes"][-1]
-            self.body.append(f"```{code}\n")
-            return
-        self.body.append("```\n")
-
-    def depart_literal_block(self, node: nodes.literal_block):
-        self.body.append("\n```\n\n")
-
-    @block_on_structural
-    def visit_option_list(self, node: nodes.option_list):
-        self._hi.push("/ ")
-
-    def depart_option_list(self, node: nodes.option_list):
-        self._hi.pop()
-
+    # =============
     @block_on_structural
     def visit_paragraph(self, node: nodes.paragraph):
         pass
@@ -281,39 +166,8 @@ class TypstTranslator(nodes.NodeVisitor):
     def depart_paragraph(self, node: nodes.paragraph):
         pass
 
-    def visit_raw(self, node: nodes.raw):
-        if "format" in node and node["format"] == "typst":
-            # NOTE: ``self.body.append(node.astext())`` does not work as expected.
-            for line in node.astext().split("\n"):
-                self.body.append(f"{line}\n")
-            self.body.append("\n")
-        raise nodes.SkipNode
-
-    def visit_reference(self, node: nodes.reference):
-        # TODO: Add case for internal links if exists.
-        href = node["refuri"]
-        self.body.append(f'#link("{href}")[')
-
-    def depart_reference(self, node: nodes.reference):
-        self.body.append("]")
-
-    # Refs: https://typst.app/docs/reference/model/quote/
-    @block_on_structural
-    def visit_block_quote(self, node: nodes.block_quote):
-        self._hi.push("  ")
-        args = []
-        attrs = list(node.findall(nodes.attribution))
-        if attrs:
-            args.append(f"attribution: [{attrs[0].astext()}]")
-            for a in attrs:
-                node.remove(a)
-        self.body.append(f"#quote({' '.join(args)})[\n")
-        self.body.append(self._hi.prefix)
-
-    def depart_block_quote(self, node: nodes.block_quote):
-        self._hi.pop()
-        self.body.append("\n]\n")
-
+    # Bullet Lists and Enumerated Lists
+    # ---------------------------------
     # Refs: https://typst.app/docs/reference/model/list/
     @block_on_structural
     def visit_bullet_list(self, node: nodes.bullet_list):
@@ -333,29 +187,36 @@ class TypstTranslator(nodes.NodeVisitor):
     def depart_enumerated_list(self, node: nodes.enumerated_list):
         self._hi.pop()
 
-    def visit_figure(self, node: nodes.figure):
-        # FIXME: Implement is complex.
-        if self._hi.is_indent_only:
-            self.body.append(self._hi.prefix)
-        self.body.append("#figure([\n")
-        self._hi.push("  ")
-        if node.first_child_matching_class(nodes.reference) is not None:
-            self.body.append(f"{self._hi.indent}")
-            self._hi.push("  ")
+    def visit_list_item(self, node: nodes.list_item):
+        self.body.append(self._hi.prefix)
 
-    def depart_figure(self, node: nodes.figure):
-        self._hi.pop()
-        self.body.append(f"{self._hi.indent})\n\n")
+    def depart_list_item(self, node: nodes.list_item):
+        if (
+            node.first_child_matching_class((nodes.bullet_list, nodes.enumerated_list))
+            is None
+        ):
+            self.body.append("\n")
 
-    # Body Subelements
+    # Definition Lists
     # ----------------
+    @block_on_structural
+    def visit_definition_list(self, node: nodes.definition_list):
+        self._hi.push("/ ")
 
-    def visit_caption(self, node: nodes.caption):
-        self.body.append(",\n")
-        self.body.append(f"{self._hi.indent}caption: [")
+    def depart_definition_list(self, node: nodes.definition_list):
+        self._hi.pop()
 
-    def depart_caption(self, node: nodes.caption):
-        self.body.append("],\n")
+    def visit_definition_list_item(self, node: nodes.definition_list_item):
+        self.body.append(self._hi.prefix)
+
+    def depart_definition_list_item(self, node: nodes.definition_list_item):
+        self.body.append("\n")
+
+    def visit_term(self, node: nodes.term):
+        pass
+
+    def depart_term(self, node: nodes.term):
+        pass
 
     def visit_classifier(self, node: nodes.classifier):
         self.body.append(" \\<")
@@ -371,17 +232,18 @@ class TypstTranslator(nodes.NodeVisitor):
     def depart_definition(self, node: nodes.definition):
         pass
 
-    def visit_definition_list_item(self, node: nodes.definition_list_item):
-        self.body.append(self._hi.prefix)
+    # Field Lists
+    # -----------
+    # Refs: https://typst.app/docs/reference/model/terms/
+    @block_on_structural
+    def visit_field_list(self, node: nodes.field_list):
+        self.body.append("#table(\n")
+        self._hi.push("  ")
+        self.body.append(f"{self._hi.indent}columns: (1fr, auto),\n")
 
-    def depart_definition_list_item(self, node: nodes.definition_list_item):
-        self.body.append("\n")
-
-    def visit_description(self, node: nodes.description):
-        self.body.append(self._hi.indent)
-
-    def depart_description(self, node: nodes.description):
-        pass
+    def depart_field_list(self, node: nodes.field_list):
+        self._hi.pop()
+        self.body.append(")")
 
     def visit_field(self, node: nodes.field):
         pass
@@ -413,20 +275,49 @@ class TypstTranslator(nodes.NodeVisitor):
             return
         self.body.append("],\n")
 
-    def visit_list_item(self, node: nodes.list_item):
-        self.body.append(self._hi.prefix)
+    # Bibliographic Fields
+    # --------------------
+    def visit_docinfo(self, node: nodes.docinfo):
+        self._hi.push("/ ")
 
-    def depart_list_item(self, node: nodes.list_item):
-        if (
-            node.first_child_matching_class((nodes.bullet_list, nodes.enumerated_list))
-            is None
-        ):
-            self.body.append("\n")
+    def depart_docinfo(self, node: nodes.docinfo):
+        self._hi.pop()
 
-    def visit_option_group(self, node: nodes.option_group):
-        text = ", ".join([option.astext() for option in node.findall(nodes.option)])
-        self.body.append(f"{text}: \\\n")
-        raise nodes.SkipNode
+    def _visit_bibliographic(self, node: nodes.Bibliographic):
+        title = node.__class__.__name__.title()
+        self.body.append(f"{self._hi.prefix}{title}: ")
+
+    def _depart_bibliographic(self, node: nodes.Bibliographic):
+        self.body.append("\n")
+
+    # Refs: https://typst.app/docs/reference/model/terms/
+    visit_address = _visit_bibliographic
+    depart_address = _depart_bibliographic
+    visit_author = _visit_bibliographic
+    depart_author = _depart_bibliographic
+    visit_contact = _visit_bibliographic
+    depart_contact = _depart_bibliographic
+    visit_copyright = _visit_bibliographic
+    depart_copyright = _depart_bibliographic
+    visit_date = _visit_bibliographic
+    depart_date = _depart_bibliographic
+    visit_organization = _visit_bibliographic
+    depart_organization = _depart_bibliographic
+    visit_revision = _visit_bibliographic
+    depart_revision = _depart_bibliographic
+    visit_status = _visit_bibliographic
+    depart_status = _depart_bibliographic
+    visit_version = _visit_bibliographic
+    depart_version = _depart_bibliographic
+
+    # Option Lists
+    # ------------
+    @block_on_structural
+    def visit_option_list(self, node: nodes.option_list):
+        self._hi.push("/ ")
+
+    def depart_option_list(self, node: nodes.option_list):
+        self._hi.pop()
 
     def visit_option_list_item(self, node: nodes.option_list_item):
         self.body.append(self._hi.prefix)
@@ -434,14 +325,69 @@ class TypstTranslator(nodes.NodeVisitor):
     def depart_option_list_item(self, node: nodes.option_list_item):
         self.body.append("\n")
 
-    def visit_term(self, node: nodes.term):
+    def visit_option_group(self, node: nodes.option_group):
+        text = ", ".join([option.astext() for option in node.findall(nodes.option)])
+        self.body.append(f"{text}: \\\n")
+        raise nodes.SkipNode
+
+    def visit_description(self, node: nodes.description):
+        self.body.append(self._hi.indent)
+
+    def depart_description(self, node: nodes.description):
         pass
 
-    def depart_term(self, node: nodes.term):
-        pass
+    # Literal Blocks
+    # --------------
+    # Refs: https://typst.app/docs/reference/text/raw/
+    def visit_literal_block(self, node: nodes.literal_block):
+        if "code" in node["classes"]:
+            code = node["classes"][-1]
+            self.body.append(f"```{code}\n")
+            return
+        self.body.append("```\n")
 
-    # Inline Elements
-    # ---------------
+    def depart_literal_block(self, node: nodes.literal_block):
+        self.body.append("\n```\n\n")
+
+    # Line Blocks
+    # -----------
+    # TODO: Implement after
+
+    # Block Quotes
+    # ------------
+    # Refs: https://typst.app/docs/reference/model/quote/
+    @block_on_structural
+    def visit_block_quote(self, node: nodes.block_quote):
+        self._hi.push("  ")
+        args = []
+        attrs = list(node.findall(nodes.attribution))
+        if attrs:
+            args.append(f"attribution: [{attrs[0].astext()}]")
+            for a in attrs:
+                node.remove(a)
+        self.body.append(f"#quote({' '.join(args)})[\n")
+        self.body.append(self._hi.prefix)
+
+    def depart_block_quote(self, node: nodes.block_quote):
+        self._hi.pop()
+        self.body.append("\n]\n")
+
+    # Doctest Blocks
+    # --------------
+    # TODO: Implement after
+
+    # Tables
+    # ------
+    # TODO: Implement after
+
+    # Explicit Markup Blocks
+    # ----------------------
+    def visit_comment(self, node: nodes.comment):
+        raise nodes.SkipNode
+
+    #
+    # Inline Markup
+    # =============
     def _enclose_content(wrapper: str):
         def _enclose(self, node: nodes.Inline):
             self.body.append(wrapper)
@@ -450,7 +396,6 @@ class TypstTranslator(nodes.NodeVisitor):
 
     # Refs: https://typst.app/docs/reference/model/emph/
     visit_emphasis, depart_emphasis = _enclose_content("_")
-
     # Refs: https://typst.app/docs/reference/model/strong/
     visit_strong, depart_strong = _enclose_content("*")
 
@@ -471,3 +416,82 @@ class TypstTranslator(nodes.NodeVisitor):
     # Refs: https://typst.app/docs/reference/model/raw/
     visit_literal = _enclose_literal("visit")
     depart_literal = _enclose_literal("depart")
+
+    def visit_reference(self, node: nodes.reference):
+        # TODO: Add case for internal links if exists.
+        href = node["refuri"]
+        self.body.append(f'#link("{href}")[')
+
+    def depart_reference(self, node: nodes.reference):
+        self.body.append("]")
+
+    # =========================================================
+    # The visitors and deparers for reStructuredText Directives
+    # =========================================================
+    #
+    # They are sorted by these rules:
+    #
+    #   * The order from contents of `reStructuredText Directives`_.
+    #   * When the node has children node types, write it nearby the parent node type.
+    #   * ``visit_`` is first, and ``depart_`` is second if it is exists.
+    #
+    # .. _reStructuredText Directives: https://docutils.org/docs/ref/rst/directives.html
+
+    # Admonitions
+    # ===========
+    # TODO: Implement after
+
+    # Images
+    # ======
+    def visit_image(self, node: nodes.image):
+        # FIXME: Implement is too complex.
+        prefix = self._hi.indent
+        suffix = "\n\n"
+        if isinstance(node.parent, nodes.figure):
+            prefix = f"{prefix}"
+            suffix = "]"
+        elif isinstance(node.parent, nodes.reference):
+            prefix = f"\n{prefix}"
+            suffix = "]"
+        self.body.append(f"{prefix}#image(\n")
+        self._hi.push("  ")
+        self.body.append(f'{self._hi.indent}"{node["uri"]}",\n')
+        if "alt" in node:
+            self.body.append(f'{self._hi.indent}alt: "{node["alt"]}",\n')
+        if "width" in node:
+            self.body.append(f"{self._hi.indent}width: {node['width']},\n")
+        self._hi.pop()
+        self.body.append(f"{self._hi.indent}){suffix}")
+        if isinstance(node.parent, nodes.reference):
+            self._hi.pop()
+
+    def visit_figure(self, node: nodes.figure):
+        # FIXME: Implement is complex.
+        if self._hi.is_indent_only:
+            self.body.append(self._hi.prefix)
+        self.body.append("#figure([\n")
+        self._hi.push("  ")
+        if node.first_child_matching_class(nodes.reference) is not None:
+            self.body.append(f"{self._hi.indent}")
+            self._hi.push("  ")
+
+    def depart_figure(self, node: nodes.figure):
+        self._hi.pop()
+        self.body.append(f"{self._hi.indent})\n\n")
+
+    def visit_caption(self, node: nodes.caption):
+        self.body.append(",\n")
+        self.body.append(f"{self._hi.indent}caption: [")
+
+    def depart_caption(self, node: nodes.caption):
+        self.body.append("],\n")
+
+    # Miscellaneous
+    # =============
+    def visit_raw(self, node: nodes.raw):
+        if "format" in node and node["format"] == "typst":
+            # NOTE: ``self.body.append(node.astext())`` does not work as expected.
+            for line in node.astext().split("\n"):
+                self.body.append(f"{line}\n")
+            self.body.append("\n")
+        raise nodes.SkipNode
