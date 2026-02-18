3a14bdd350901cf4e4f4189bd5b8d44fdc3d00de
Kazuya Takei <myself@attakei.net>
2026-02-19 03:29:53 +0900

    doc: Update header components

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,15 @@ html_theme = "shibuya"
 html_static_path = ["_static"]
 html_theme_options = {
     "announcement": "This is not yet published on PyPI.",
+    "github_url": "https://github.com/atsphinx/rst2typst",
+    "nav_links": [
+        {
+            "title": "It works on atsphinx project",
+            "url": "https://atsphinx.github.io/",
+            "external": True,
+        },
+    ],
+    "nav_links_align": "right",
 }
 
 # -- Options for extensions
