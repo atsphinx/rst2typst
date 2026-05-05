a3a658a2c8561a3a006274f439fe4f8f667b3a90
Kazuya Takei <myself@attakei.net>
2026-05-05 20:16:42 +0900

    feat: Add flag to install forcely

--- src/rst2typst/cli/rst2typstpdf.py
+++ src/rst2typst/cli/rst2typstpdf.py
@@ -2,10 +2,8 @@
 
 from docutils.core import publish_cmdline
 
-from ..package import install_package
 from ..pdf import Writer
 
 
 def main():
-    install_package()
     publish_cmdline(writer=Writer())
--- src/rst2typst/pdf.py
+++ src/rst2typst/pdf.py
@@ -1,13 +1,33 @@
 """PDF handler."""
 
 import typst
+from docutils.frontend import validate_boolean
 
+from .package import install_package
 from .writer import Writer as BaseWriter
 
 
 class Writer(BaseWriter):
     format = "pdf"
 
+    settings_spec = BaseWriter.settings_spec + (
+        "TypstPDF Writer Options",
+        None,
+        (
+            (
+                "Install Typst local package forcely.",
+                ["--force-install-package"],
+                {
+                    "action": "store_true",
+                    "dest": "force_install_package",
+                    "default": False,
+                    "validator": validate_boolean,
+                },
+            ),
+        ),
+    )
+
     def translate(self):
         super().translate()
+        install_package(force=self.document.settings.force_install_package)
         self.output = typst.compile(self.output.encode())
